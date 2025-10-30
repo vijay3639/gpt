24ba2fb301f193c254db5f0573d16c1875b4bbcf
vijay3639 <vijay@gmail.com>
2025-10-30 17:53:33 +0530

    Project

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "newk8svjbuck" # Replace with your actual S3 bucket name
+    bucket = "vijaycdcp2025" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-west-2"
   }
 }
