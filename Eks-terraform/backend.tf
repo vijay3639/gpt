a4d1a28b3a27fecedf00e1b648b25344d080cfab
vijay3639 <vijay@gmail.com>
2026-01-28 16:31:01 +0530

    test

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "k8sdemovijay" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-west-2"
   }
 }
